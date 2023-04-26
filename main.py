af2e4f6bf8d9f313fb83a6ef3eb2b2874c7d4332
kiyora <kiyora@kakao.com>
2023-04-26 16:19:34 +0900

    Notepad template

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter.filedialog import *
+
+window = Tk()
+window.title("Notepad")
+window.geometry("400x400")
+window.resizable(False, False)
+
+menu = Menu(window)
+menu_1 = Menu(menu, tearoff = 0) #플로팅 효과
+menu_1.add_command(label="새파일")
+menu_1.add_command(label="저장")
+menu_1.add_separator()
+menu_1.add_command(label="종료", command=window.destroy) #윈도우가 꺼짐
+menu.add_cascade(label="파일", menu=menu_1) #하위메뉴 추가
+
+menu_2 = Menu(menu, tearoff = 0)
+menu_2.add_command(label="만든이")
+menu.add_cascade(label="만든이", menu=menu_2)
+
+window.config(menu=menu)
+window.mainloop()
